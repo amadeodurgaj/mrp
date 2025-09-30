e836260f6d5e3f5b0bc8d44e7eaf12209e13bb4e
Amadeo Durgaj <thundahlovesjojo@gmail.com>
2025-09-30 21:45:38 +0200

    Change to media for LSP compliance

--- mrp/src/main/java/org/mrp/model/Media.java
+++ mrp/src/main/java/org/mrp/model/Media.java
@@ -1,16 +1,6 @@
 package org.mrp.model;
 
-abstract class Media {
+public abstract class Media {
     protected String title;
-    protected Integer releaseYear;
-
     public abstract String getType();
-
-    public Integer getReleaseYear() {
-        return releaseYear;
-    }
-
-    public void setReleaseYear(Integer year) {
-        this.releaseYear = year;
-    }
 }
